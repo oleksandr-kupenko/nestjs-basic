8360e32197d671583db8ee7369666075ccd8607d
oleksandr-kupenko <xoxol4uk22@gmail.com>
2024-01-18 12:03:10 +0200

    move cookies key to env

--- src/app.module.ts
+++ src/app.module.ts
@@ -50,10 +50,12 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {
+  }
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(
       cookieSession({
-      keys: ['custom-string']
+      keys: [this.configService.get('COOKIE_KEY')]
     })).forRoutes('*');
   }
 }
